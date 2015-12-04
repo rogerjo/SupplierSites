5c60713f829f740d0367f1cd4363eb66bf6d08e6
Roger Jönsson <rogerjoensson@outlook.com>
2015-12-04 09:58:12 +0100

    Final

--- SupplierSitesFileShuffler/SupplierSitesFileShuffler/Helper.cs
+++ SupplierSitesFileShuffler/SupplierSitesFileShuffler/Helper.cs
@@ -1,25 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.IO;
 using Microsoft.SharePoint.Client;
-using Microsoft.SharePoint.Client.DocumentSet;
-using SupplierSitesFileShuffler;
-using MahApps.Metro.Controls;
-using System.Threading;
-using System.ComponentModel;
 
 namespace SupplierSitesFileShuffler
 {
--- SupplierSitesFileShuffler/SupplierSitesFileShuffler/MainWindow.xaml.cs
+++ SupplierSitesFileShuffler/SupplierSitesFileShuffler/MainWindow.xaml.cs
@@ -3,22 +3,11 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.SharePoint.Client;
-using Microsoft.SharePoint.Client.DocumentSet;
 using SupplierSitesFileShuffler;
 using MahApps.Metro.Controls;
-using System.Threading;
 using System.ComponentModel;
 
 namespace Renamer
