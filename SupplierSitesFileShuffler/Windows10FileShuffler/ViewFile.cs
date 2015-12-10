10b7d260852c255807afc62b4af88f8ab274519b
Roger Jönsson <rogerjoensson@outlook.com>
2015-12-10 12:23:49 +0100

    before Help

---
+++ SupplierSitesFileShuffler/Windows10FileShuffler/ViewFile.cs
@@ -0,0 +1,25 @@
+﻿namespace SupplierSitesFileShuffler
+{
+    public class ViewFile
+    {
+        public string Extension { get; set; }
+        public string SourceLocation { get; set; }
+        public string FileSize { get; set; }
+        public string PartNo { get; set; }
+        public string CopySite { get; set; }
+        public string FileName { get; set; }
+        public bool SiteFound { get; set; }
+        public string Supplier { get; set; }
+        public string Version { get; set; }
+        public string Status { get; set; }
+        public string NewFileName { get; set; }
+
+
+
+        public ViewFile()
+        {
+
+        }
+
+    }
+}
